91d9daf463fa7ca665417755b122711f25c228ee
matheusn_ramos <matheus.ramos@nappsolutions.com>
2021-03-27 08:58:00 -0300

    modificando semana6

--- NappAcademy/semana6/BancoNapp/contas/ContaPessoaJuridica.py
+++ NappAcademy/semana6/BancoNapp/contas/ContaPessoaJuridica.py
@@ -2,4 +2,16 @@ from BancoNapp.contas.Conta import Conta
 
 
 class ContaPessoaJuridica(Conta):
-    pass
+    """
+    Classe representa a conta corrente de pessoa jurídica.
+    Limite padrão da conta: R$ 500,00
+    Args:
+        Conta ([type]): [description]
+    """
+    def __init__(self,  **kwargs):
+        """
+        Construtor da classe ContaPessoaJuridica.
+        """
+        super(ContaPessoaJuridica, self).__init__(**kwargs)
+        self.empresa = kwargs.get('empresa', '')
+        self.limite = kwargs.get('limite', 1500)
--- NappAcademy/semana6/BancoNapp/contas/ContaPoupanca.py
+++ NappAcademy/semana6/BancoNapp/contas/ContaPoupanca.py
@@ -2,4 +2,18 @@ from BancoNapp.contas.Conta import Conta
 
 
 class ContaPoupanca(Conta):
-   pass
+   def __init__(self,  **kwargs):
+      """
+      Construtor da classe ContaPoupanca.
+      """
+      super(ContaPoupanca, self).__init__(**kwargs)
+      self.profissao = kwargs.get('profissao', '')
+      self.limite = kwargs.get('limite',0)
+      
+
+
+   def rendimento_aniversario(self, valor):
+      if valor < 0 or valor > 1:
+         raise ValueError('Os juros precisam ser entre 0 (0%) e 1 (100%).')
+      self.saldo += self.saldo*valor
+
